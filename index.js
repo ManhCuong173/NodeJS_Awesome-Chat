2384b1aaa40eba9b66cca05e06113e1d7717f11e
ManhCuong173 <daomanhcuong173@gmail.com>
2019-08-07 22:16:33 +0700

    [1.0.0] Initial hellworld

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+var port = 3000;
+var hostname = "localhost";
+
+app.get('/helloworld', (req,res) => {
+  res.send('<h1> Hello World </h1>');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Hello Manh Cuong at : ${hostname} : ${port}`);
+})
