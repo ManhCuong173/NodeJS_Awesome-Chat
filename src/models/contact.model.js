adb236751593571ec95bb9dfd44c38ff87686624
ManhCuong173 <daomanhcuong173@gmail.com>
2019-08-08 11:41:59 +0700

    [1.0.0] Define model schemas

---
+++ src/models/contact.model.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+let  Schema = mongoose.Schema;
+
+let ContactSchema = new Schema({
+  userId: String,
+  contactId: String,
+  status: {type: String, default: false},
+  createdAt: {type: Number, default: Date.now},
+  updatedAt: {type: Number, default: Date.now},
+  deletedAt: {type: Number, default: Date.now},
+});
+
+module.exports = mongoose.model("contact", ContactSchema);
