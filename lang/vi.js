8cea02e1230ebc1d6d2503e40a68da7cb7a8a228
ManhCuong173 <daomanhcuong173@gmail.com>
2019-08-10 11:38:02 +0700

    [1.0.0] Validation Register Information

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation = {
+  email_incorrect: "Email phải có dạng example@manhcuongdev.com",
+  gender_incorrect: "Đừng có nghịch tml!",
+  password_incorrect: "Mật khẩu phải chứa ít nhất 8 kí tự, bao gồm chữ hoa, thường, số, kí tự đặc biệt",
+  password_confirmation_incorrect: "Mật khẩu không trùng. Vui lòng nhập lại"
+}
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,21 @@
-let authController = (req,res) => {
+import {validationResult} from 'express-validator/check';
+
+let getLoginRegister = (req,res) => {
   res.render('auth/master');
 };
 
+let postRegister = (req,res) => {
+  let errorsArr = [];
+  let validationErrors = validationResult(req);
+  if(!validationErrors.isEmpty()){
+    let errors = Object.values(validationErrors.mapped());
+    errors.forEach(element => {
+      errorsArr.push(element.msg);
+    });
+    console.log(errorsArr);
+  }
+}
 module.exports = {
-  authController:authController
+  getLoginRegister: getLoginRegister,
+  postRegister: postRegister
 };
